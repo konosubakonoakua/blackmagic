9455773521ed2e08a8db3be461215d014ed757ef
dragonmux <git@dragonmux.network>
2023-03-28 04:06:07 +0100

    esp32c3: Implemented discovery of and memory maps for the RAM of the ESP32-C3

--- src/target/target_probe.h
+++ src/target/target_probe.h
@@ -94,6 +94,7 @@ bool rp_probe(target_s *target);
 bool renesas_probe(target_s *target);
 bool imxrt_probe(target_s *target);
 bool zynq7_probe(target_s *target);
+bool esp32c3_probe(target_s *target);
 
 void lpc55_dp_prepare(adiv5_debug_port_s *dp);
 bool esp32c3_target_prepare(target_s *target);
